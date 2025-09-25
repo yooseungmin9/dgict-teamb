94b849a9bf318eda6ef1b05de5f758e0c1a39e38
“yooseungmin9” <2win0min@naver.com>
2025-09-25 17:49:56 +0900

    1

---
+++ news/src/test/java/com/bgroup/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgroup.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
