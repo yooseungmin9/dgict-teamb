aa3fa8161c094fea6209e928316afdc9de58ab3c
Yoo <2win0min@naver.com>
2025-10-01 17:42:34 +0900

    1001-유승민-중간저장

--- fast-api/run_many.py
+++ fast-api/run_many.py
@@ -4,11 +4,10 @@ import subprocess, sys, signal, os
 # ❗️여기에 서버 목록을 추가하세요.
 #   (작업폴더, "모듈:앱", 포트, [선택]가상환경 파이썬 경로)
 SERVERS = [
-    (r"/Users/yoo/bootcamp_dgict/dgict-teamb/fast-api/챗봇서비스", "chat_server2:app", 8002, None),
-    (r"/Users/yoo/bootcamp_dgict/dgict-teamb/fast-api/FastAPI_도서추천", "main:app", 8003, None),
-    (r"/Users/yoo/bootcamp_dgict/dgict-teamb/fast-api/FastAPI_도서추천", "youtube_api:app", 8004, None),
-    (r"/Users/yoo/bootcamp_dgict/dgict-teamb/fast-api/요약기능_전날브리핑", "main:app", 8005, None),
-    (r"/Users/yoo/bootcamp_dgict/dgict-teamb/fast-api/trend", "api.main:app", 8006, None),
+    (r"C:\dgict-teamb\fast-api\FastAPI_도서추천", "main:app", 8001),
+    (r"C:\dgict-teamb\fast-api\FastAPI_도서추천", "youtube_api:app", 8004, None),
+    (r"C:\dgict-teamb\fast-api\요약기능_전날브리핑", "main:app", 8005, None),
+    # (r"/Users/yoo/bootcamp_dgict/dgict-teamb/fast-api/trend", "api.main:app", 8006, None),
     # 예시) 다른 서비스 추가
     # (r"C:\Dgict_TeamB_Project\fastapi22\FastAPI_다른서비스", "main:app", 8001, r"C:\Dgict_TeamB_Project\fastapi22\.venv\Scripts\python.exe"),
 ]
